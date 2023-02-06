355d01b3da89458795ad8970ef30463c915f315f
Feed Updater <actions@github.com>
2023-02-06 08:32:29 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -279,7 +279,7 @@ const feeds = [
     "id": "historiske_kjendiser",
     "title": "De 10 siste fra Historiske kjendiser",
     "season": null,
-    "enabled": false
+    "enabled": true
   },
   {
     "id": "hoera",
