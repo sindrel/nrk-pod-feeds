d0e96bb4a328b9ed81dea03f5a65db60c2f1b229
Feed Updater <actions@github.com>
2023-05-12 07:22:21 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -489,7 +489,7 @@ const feeds = [
     "id": "kongerekka",
     "title": "De 10 siste fra Kongerekka",
     "season": null,
-    "enabled": false
+    "enabled": true
   },
   {
     "id": "koselig",
