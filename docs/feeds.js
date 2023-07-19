4cd06f12a647d9b52737af5ba548898cca9cda22
Feed Updater <actions@github.com>
2023-07-19 07:22:31 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -1190,7 +1190,7 @@ const feeds = [
     "id": "trygdekontoret",
     "title": "De 10 siste fra Trygdekontoret",
     "season": null,
-    "enabled": true
+    "enabled": false
   },
   {
     "id": "turister",
