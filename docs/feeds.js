a22395acb4ce337e981f8a6d6bbaa3f890877eb4
Feed Updater <actions@github.com>
2024-12-19 07:29:35 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -1033,7 +1033,7 @@ const feeds = [
     "id": "radiodokumentaren",
     "title": "De 10 siste fra Historier fra virkeligheten",
     "season": "LATEST_SEASON",
-    "enabled": true
+    "enabled": false
   },
   {
     "id": "radiolegen",
