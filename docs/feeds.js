d3936b57079d5eac7e6c27e068c69042eef374db
Feed Updater <actions@github.com>
2025-05-20 06:44:48 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -1426,7 +1426,7 @@ const feeds = [
     "id": "the_kaass_furuseths",
     "title": "De 10 siste fra The Kåss Furuseths",
     "season": null,
-    "enabled": true
+    "enabled": false
   },
   {
     "id": "timeout",
