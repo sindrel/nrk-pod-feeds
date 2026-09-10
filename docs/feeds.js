a07cc80951882b36ba7edaa41dd34e213e705b52
Feed Updater <actions@github.com>
2026-09-10 05:59:52 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -824,7 +824,7 @@ const feeds = [
     "id": "lindmo_og_co",
     "title": "De 10 siste fra Lindmo og Co",
     "season": null,
-    "enabled": false
+    "enabled": true
   },
   {
     "id": "loensj_med_rune_nilson",
